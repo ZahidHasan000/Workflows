55c8d9620e52e01e3594bc1f38e0845e4f0674b3
ZahidHasan000 <h.zahid007.zh50@gmail.com>
2022-12-15 00:33:46 +0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/app.js',
+//   output: {
+//     filename: 'app.js',
+//     path: path.resolve(__dirname, 'assets', 'scripts'),
+//     publicPath: 'assets/scripts/'
+//   },
+//   devtool: 'eval-cheap-source-map'
+
+//   // devServer: {
+//   //   contentBase: './'
+//   // }
+//   // devServer: {
+//   //   static: {
+//   //     directory: path.join(__dirname, 'public'),
+//   //   },
+//   //   compress: true,
+//   //   port: 9000,
+//   // },
+// };
